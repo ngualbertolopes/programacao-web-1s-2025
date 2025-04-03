4267177d6daa4ebef0ff7d8cb919c205680b88f6
Nathália Gualberto lopes <146035955+ngualbertolopes@users.noreply.github.com>
2025-04-03 15:45:23 -0300

    Atividade3

---
+++ Calculadora/Calculadora.js
@@ -0,0 +1,19 @@
+
+
+export function somar(a, b) {
+    return a + b;
+}
+
+export function subtrair(a, b) {
+    return a - b;
+}
+
+export function multiplicar(a, b) {
+    return a * b;
+}
+
+export function dividir(a, b) {
+    return a / b;
+}
+
+export const PI = 3.14;
---
+++ Calculadora/package-lock.js
@@ -0,0 +1,13 @@
+{
+    "name": "app_calculadora",
+    "version": "1.0.0",
+    "description": "Aplicação web de calculadora com rotas para operações básicas.",
+    "main": "index.js",
+    "scripts": {
+      "start": "node index.js"
+    },
+    "dependencies": {
+      "express": "^4.18.2"
+    }
+  }
+  
